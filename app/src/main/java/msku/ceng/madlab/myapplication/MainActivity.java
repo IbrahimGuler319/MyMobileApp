62134fde89ecd8a3565b85936749fb5734231fb7
U200709065 <brhmguler319@gmail.com>
2024-01-03 02:46:37 +0300

    Theme

--- app/src/main/java/msku/ceng/madlab/myapplication/MainActivity.java
+++ app/src/main/java/msku/ceng/madlab/myapplication/MainActivity.java
@@ -1,65 +1,22 @@
 package msku.ceng.madlab.myapplication;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import msku.ceng.madlab.myapplication.R;
-import msku.ceng.madlab.myapplication.Register;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void route() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            FirebaseFirestore.getInstance()
-                    .collection("users")
-                    .document(user.getUid())
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if (task.isSuccessful()) {
-                                DocumentSnapshot document = task.getResult();
-                                if (document.exists()) {
-                                    Intent intent = new Intent(MainActivity.this, MainMap.class);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    Log.d(TAG, "Document does not exist in the database");
-                                }
-                            } else {
-                                Log.d(TAG, "Failed to retrieve document: ", task.getException());
-                            }
-                        }
-                    });
-        } else {
-            Log.d(TAG, "User is not logged in");
-        }
-    }
-
-    Button btnSignUp, btnLogin;
+    private Button btnSignUp, btnLogin;
     private EditText editEmail, editPassword;
     private String txtEmail, txtPassword;
     private FirebaseAuth mAuth;
@@ -70,13 +27,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
         btnSignUp = findViewById(R.id.signupbtn);
         btnLogin = findViewById(R.id.btnLogin);
-
         editEmail = findViewById(R.id.loginEmail);
         editPassword = findViewById(R.id.loginPassword);
-
-        mAuth = FirebaseAuth.getInstance();
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,22 +43,20 @@ public class MainActivity extends AppCompatActivity {
 
                 if (!TextUtils.isEmpty(txtEmail) && !TextUtils.isEmpty(txtPassword)) {
                     mAuth.signInWithEmailAndPassword(txtEmail, txtPassword)
-                            .addOnSuccessListener(MainActivity.this, new OnSuccessListener<AuthResult>() {
-                                @Override
-                                public void onSuccess(AuthResult authResult) {
+                            .addOnCompleteListener(MainActivity.this, task -> {
+                                if (task.isSuccessful()) {
                                     mUser = mAuth.getCurrentUser();
 
-                                    System.out.println("Kullanıcı Adı:" + mUser.getDisplayName());
-                                    System.out.println("Kullanıcı Email:" + mUser.getEmail());
-                                    System.out.println("Kullanıcı Uid:" + mUser.getUid());
-
-                                    Intent intent = new Intent(MainActivity.this, MainMap.class);
-                                    startActivity(intent);
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    if (mUser != null) {
+                                        Toast.makeText(MainActivity.this, "Logged in successfully!", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(MainActivity.this, MainMap.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "User authentication failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                                 }
                             });
                 } else {
@@ -117,7 +72,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(signUp);
             }
         });
-
-        route();
     }
 }
+
